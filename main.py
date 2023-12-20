aaa7d3095e19d567786231b37358a5efa67f66bf
ejders-bzz <ejders@bzz.ch>
2023-12-20 11:14:36 +0100

    added Gui

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import requests
+import tkinter as tk
 
 
-def main():
-
+def get_joke():
     response = requests.get(f'https://api.chucknorris.io/jokes/random')
     print(response.status_code)
     data = response.json()
     joke = data['value']
     print(joke)
+
+
+def main():
+
+    root = tk.Tk()
+    root.geometry('200x200')
+    root.title('API hub')
+    chuck_norris_btn = tk.Button(root, text='Chuck Norris', command=get_joke)
+    chuck_norris_btn.grid(column=1, row=1)
+    root.mainloop()
 
 
 if __name__ == '__main__':
